7158403d74e4c455ac775ee06703d3f8062371c5
M <141963542+kings591998@users.noreply.github.com>
2026-05-30 01:25:57 +0300

    Create bots.js
    Signed-off-by: M <141963542+kings591998@users.noreply.github.com>

---
+++ assets/js/config/bots.js
@@ -0,0 +1,23 @@
+// ============================================================
+//  config/bots.js
+//  الوظيفة: تكوين بوتات تيليجرام وقنواتها - عدّل هنا فقط
+// ============================================================
+
+const BOTS_CONFIG = [
+  {
+    name: '23bot',
+    token: '8931105902:AAFhk6rD8AE4xthwinDOQCrdIfqZLXEILD8',
+    channelId: '-1003969417470'
+  },
+  {
+    name: '24bot',
+    token: '8981308591:AAEpE0f47SIilWIutd1-mBEqJ8dBGRTbaEo',
+    channelId: '-1003423766965'
+  },
+  {
+    name: '25bot',
+    token: '8993825217:AAE30r5cZqWG3JFaYH0enIeUDFqFYWYiNsc',
+    channelId: '-1003840767722'
+  }
+  // لإضافة بوت جديد، انسخ أحد العناصر أعلاه وعدّل البيانات
+];
